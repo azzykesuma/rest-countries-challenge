4310ac245eccb74061aeb2536a7db85375be54f8
azzykesuma <official.azzy@gmail.com>
2024-07-17 11:33:51 +0700

    initial setup

--- pages/[country].tsx
+++ pages/[country].tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const country = () => {
   return (
     <div>
-      wello
+      country
     </div>
   )
 }
 
-export default Home
+export default country
